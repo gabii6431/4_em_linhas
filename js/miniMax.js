68df728ca936f20d121d1237538dd28a2d22e3ed
gabii6431 <gabii6431@gmail.com>
2019-06-06 15:46:04 -0300

    Método da Nota (em andamento)

--- js/miniMax.js
+++ js/miniMax.js
@@ -83,8 +83,103 @@ class MiniMax{
         }
         return min;
     }
+    geraCombinações(){
+        let arrayCombo = [];
+        let combo = [];
+        //primeiro pega as verticais
+        for(let i = 0; i < estado.tamLinha - 3; i++){
+            for(let j = 0; j < estado.tamColuna; j++){
+                for(let k = 0; k < 4; k++){
+                    combo[k] = matrizTabuleiro[i+k][j];
+                }
+                arrayCombo.push(combo.slice(0));
+            }
+        }
+
+        //agora as horizontais
+        for(let i = 0; i < estado.tamLinha; i++){
+            for(let j = 0; j < estado.tamColuna - 3; j++){
+                for(let k = 0; k < 4; k++){
+                    let combo[k] = matrizTabuleiro[i][j+k];
+                }
+                arrayCombo.push(combo.slice(0));
+            }
+        }
+
+        //diagonais segundo quadrante
+        for(let i = 0; i < estado.tamLinha - 3; i++){
+            for(let j = 0; j < estado.tamColuna - 3; j++){
+                for(let k = 0; k < 4; k++){
+                    let combo[k] = matrizTabuleiro[i+k][j+k];
+                }
+                arrayCombo.push(combo.slice(0));
+            }
+        }
+
+        //diagonais primeiro quadrante
+        for(let i = 3; i < estado.tamLinha; i++){
+            for(let j = 0; j < estado.tamColuna - 3; j++){
+                for(let k = 0; k < 4; k++){
+                    let combo[k] = matrizTabuleiro[i-k][j+k];
+                }
+                arrayCombo.push(combo.slice(0));
+            }
+        }
+        return arrayCombo;
+    }
     calculaNota(){
-        return 0;
+		
+		for(let j = 0; j < estado.tamColuna; j++) for(let i = estado.tamLinha - 1; i >=0 ; i--){
+			if(matrizTabuleiro == 0) for(let count = 2 ; i>=0; i--, count++){
+				matrizTabuleiro[i][j] = count;
+			}
+		}
+
+		//X -> PC Z -> JOGADOR
+
+		//XXX2 -> NOTA 5
+
+		//XXX3 -> NOTA 2000
+
+		//XXX4 -> NOTA 500
+
+		//XXX(>5) -> NOTA 400
+
+		//ZZZ2 -> NOTA -10000
+
+		//ZZZ3 -> NOTA -4000
+		
+		//ZZZ4 -> NOTA -500
+
+		//ZZZ(>5) NOTA -400
+
+		//XX2(>1) -> NOTA 100
+
+		//XX3(>1)  -> NOTA 1500
+
+		//XX4(>1)  -> NOTA 
+
+		//XX(>5)(>1)  -> NOTA 
+
+		//ZZ2(>1)  -> NOTA 
+
+		//ZZ3(>1)  -> NOTA 
+		
+		//ZZ4(>1)  -> NOTA 
+
+		//ZZ(>5)(>1)  NOTA 
+
+		//CASOS COM LACUNA
+
+		//X00X
+
+		//X0XX
+
+
+		//AO FINALIZAR O CALCULO DE NOTA É MELHOR RETORNAR O TABULEIRO AO ESTADO ORIGINAL (nao sei se ele é usado pra gerar o novo deopois de calcular a nota ainda)
+		for(let j = 0; j < estado.tamColuna; j++) for(let i = 0; i < estado.tamLinha; i++) if(matrizTabuleiro[i][j] > 1) matrizTabuleiro[i][j] = 0;
+
+		return nota;
     }
     // max(estado, nivel)
     // {
